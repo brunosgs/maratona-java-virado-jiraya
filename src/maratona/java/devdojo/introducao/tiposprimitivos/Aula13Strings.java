79064dce0838629990a6189907c16bdf7b67f096
Bruno Gomes <brunosoges@gmail.com>
2022-08-26 08:32:24 -0300

    learning(PrimitiveDataType): studies on primitive data types

---
+++ src/maratona/java/devdojo/introducao/tiposprimitivos/Aula13Strings.java
@@ -0,0 +1,14 @@
+package maratona.java.devdojo.introducao.tiposprimitivos;
+
+/* String é um tipo por referência. Os tipos por referência, são classes que especificam os
+ * tipos de objeto Strings, Arrays Primitivos e Objetos
+ * */
+public class Aula13Strings {
+
+	public static void main(String[] args) {
+		String nome = "Bruno";
+
+		System.out.println("O nome é: " + nome);
+	}
+
+}
