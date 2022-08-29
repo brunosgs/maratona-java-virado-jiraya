0954df24f577ca3269f2ac9e4588e89f84e81ad2
Bruno Gomes <brunosoges@gmail.com>
2022-08-29 13:15:04 -0300

    learning(Operators): Study on arithmetic and comparison operators

---
+++ src/maratona/java/devdojo/introducao/operadores/Aula15Aritimeticos.java
@@ -0,0 +1,21 @@
+package maratona.java.devdojo.introducao.operadores;
+
+
+public class Aula15Aritimeticos {
+
+	public static void main(String[] args) {
+		// Operadores: soma (+), subtração(-), multiplicação(*) e divisão(/)
+		int numero1 = 10;
+		int numero2 = 20;
+
+		int resultado = numero1 + numero2;
+
+		System.out.println(numero1 + numero2);
+		System.out.println(numero1 - numero2);
+		System.out.println(numero1 * numero2);
+		System.out.println(numero1 / numero2); // A divisão de 10 / 20 = 0, porque foi definido como um tipo inteiro
+
+		System.out.println(resultado);
+	}
+
+}
