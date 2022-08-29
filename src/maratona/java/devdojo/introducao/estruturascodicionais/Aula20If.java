183424e1f91cdf7d2fa24ac998016b2138a56f1f
Bruno Gomes <brunosoges@gmail.com>
2022-08-29 18:05:42 -0300

    learning(Conditions): Study on if, else if conditionals and ternary operator

---
+++ src/maratona/java/devdojo/introducao/estruturascodicionais/Aula20If.java
@@ -0,0 +1,19 @@
+package maratona.java.devdojo.introducao.estruturascodicionais;
+
+public class Aula20If {
+
+	public static void main(String[] args) {
+		int idade = 15;
+		boolean isAutorizadoComprarBebida = idade >= 18;
+
+		if (isAutorizadoComprarBebida) {
+			System.out.println("Autorizado a comprar bebida alcólica!");
+		}
+
+		// A forma de leitura disso seria, "Se não é autorizado a comprar bebida então faça" que é igual a "!isAutorizadoComprarBebida"
+		if (!isAutorizadoComprarBebida) {
+			System.out.println("Não autorizado a comprar bebida alcólica!");
+		}
+	}
+
+}
