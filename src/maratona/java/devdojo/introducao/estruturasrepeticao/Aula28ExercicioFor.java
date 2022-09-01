4a43c26c630eeac5facc099efaef2bdd4247590a
Bruno Gomes <brunosoges@gmail.com>
2022-09-01 08:16:59 -0300

    learning(ControlStructureRepetition): Study on the types of while, do while, for repetition structures

---
+++ src/maratona/java/devdojo/introducao/estruturasrepeticao/Aula28ExercicioFor.java
@@ -0,0 +1,14 @@
+package maratona.java.devdojo.introducao.estruturasrepeticao;
+
+// Imprima todos os número pares até 0 até 1000000
+public class Aula28ExercicioFor {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 1000000; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
