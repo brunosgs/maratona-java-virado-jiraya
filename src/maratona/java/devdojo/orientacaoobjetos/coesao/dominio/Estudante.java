d59d6de652c7f93d988f3828f6e128f81a88c174
Bruno Gomes <brunosoges@gmail.com>
2022-09-05 08:24:51 -0300

    learning(Cohesion): exemplifying cohesion

---
+++ src/maratona/java/devdojo/orientacaoobjetos/coesao/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package maratona.java.devdojo.orientacaoobjetos.coesao.dominio;
+
+public class Estudante {
+	public String nome;
+	public int idade;
+	public char sexo;
+}
---
+++ src/maratona/java/devdojo/orientacaoobjetos/coesao/dominio/Professor.java
@@ -0,0 +1,7 @@
+package maratona.java.devdojo.orientacaoobjetos.coesao.dominio;
+
+public class Professor {
+	public String nome;
+	public int idade;
+	public char sexo;
+}
