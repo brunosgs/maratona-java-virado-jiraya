ee7cc67f73d40fc3bd1393d8bb0d07ca5ce9b1c9
Bruno Gomes <brunosoges@gmail.com>
2022-09-08 22:24:01 -0300

    learning(AccessModifier): public, private, getters and setters

---
+++ src/maratona/java/devdojo/orientacaoobjetos/modifcadoracesso/test/Aula56PrivateGetSet02.java
@@ -0,0 +1,17 @@
+package maratona.java.devdojo.orientacaoobjetos.modifcadoracesso.test;
+
+import maratona.java.devdojo.orientacaoobjetos.modifcadoracesso.dominio.Funcionario;
+
+public class Aula56PrivateGetSet02 {
+
+	public static void main(String[] args) {
+		Funcionario funcionario = new Funcionario();
+
+		funcionario.setNome("João");
+		funcionario.setIdade(30);
+		funcionario.setSalarios(new double[] { 1200, 1600, 2772.69 });
+
+		funcionario.imprime();
+	}
+
+}
