d2a197cb9673f25a6ecba0eb3cbb8ece930256c0
Bruno Gomes <brunosoges@gmail.com>
2022-09-12 08:38:16 -0300

    style: comment adjustment

--- src/maratona/java/devdojo/orientacaoobjetos/blocoinicializacao/dominio/Anime.java
+++ src/maratona/java/devdojo/orientacaoobjetos/blocoinicializacao/dominio/Anime.java
@@ -3,7 +3,7 @@ package maratona.java.devdojo.orientacaoobjetos.blocoinicializacao.dominio;
 /*
  	Sequência da construção de um objeto:
 
- 	1. Alocado espaço em mémoria para o objeto;
+ 	1. Alocado espaço em memória para o objeto;
  	2. Cada atributo de classe é criado e inicializado com valores default ou o que for atribuido a eles;
  	3. Bloco de inicialização é executado;
  	4. Construtor é inicializado e executado.
