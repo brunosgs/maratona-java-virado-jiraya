2ca26584897cee9f2935e6a0a04c83478ae8b580
Bruno Gomes <brunosoges@gmail.com>
2022-09-13 16:29:53 -0300

    learning(Association): bidirectional

---
+++ src/maratona/java/devdojo/orientacaoobjetos/associacao/bidirecional/dominio/Jogador.java
@@ -0,0 +1,38 @@
+package maratona.java.devdojo.orientacaoobjetos.associacao.bidirecional.dominio;
+
+public class Jogador {
+	private String nome;
+	private Time time;
+
+	public Jogador(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Time getTime() {
+		return time;
+	}
+
+	public void setTime(Time time) {
+		this.time = time;
+	}
+
+	public void imprime() {
+		System.out.println(" -- JOGADOR --");
+		System.out.println(this.nome);
+
+		if (this.time == null) {
+			return;
+		}
+
+		System.out.println("Time: " + this.time.getNome() + "\n");
+	}
+
+}
