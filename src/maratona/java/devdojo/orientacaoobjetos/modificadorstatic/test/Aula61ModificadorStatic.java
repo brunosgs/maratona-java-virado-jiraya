de75c3c84b4fdfcc519f9db2fa5d26c56c98e9c5
Bruno Gomes <brunosoges@gmail.com>
2022-09-12 21:27:44 -0300

    learning(StaticModifier): using the static modifier

---
+++ src/maratona/java/devdojo/orientacaoobjetos/modificadorstatic/test/Aula61ModificadorStatic.java
@@ -0,0 +1,19 @@
+package maratona.java.devdojo.orientacaoobjetos.modificadorstatic.test;
+
+import maratona.java.devdojo.orientacaoobjetos.modificadorstatic.dominio.Carro;
+
+public class Aula61ModificadorStatic {
+
+	public static void main(String[] args) {
+		Carro carro01 = new Carro("BMW", 280);
+		Carro carro02 = new Carro("Mercedes", 275);
+		Carro carro03 = new Carro("Audi", 290);
+
+		Carro.velocidadeLimite = 180;
+
+		carro01.imprime();
+		carro02.imprime();
+		carro03.imprime();
+	}
+
+}
