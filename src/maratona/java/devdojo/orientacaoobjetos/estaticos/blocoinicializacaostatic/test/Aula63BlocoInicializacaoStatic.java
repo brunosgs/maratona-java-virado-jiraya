a85c818b517e356b8c40dfc7ff09fb106686dc32
Bruno Gomes <brunosoges@gmail.com>
2022-09-13 13:45:23 -0300

    learning(StaticInitializerBlock): using the static initializer block

---
+++ src/maratona/java/devdojo/orientacaoobjetos/estaticos/blocoinicializacaostatic/test/Aula63BlocoInicializacaoStatic.java
@@ -0,0 +1,14 @@
+package maratona.java.devdojo.orientacaoobjetos.estaticos.blocoinicializacaostatic.test;
+
+import maratona.java.devdojo.orientacaoobjetos.estaticos.blocoinicializacaostatic.dominio.Anime;
+
+public class Aula63BlocoInicializacaoStatic {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Anime anime01 = new Anime();
+		Anime anime02 = new Anime();
+		Anime anime03 = new Anime();
+	}
+
+}
