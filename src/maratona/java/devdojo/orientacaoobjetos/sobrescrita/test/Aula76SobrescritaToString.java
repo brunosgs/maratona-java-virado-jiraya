f213b870044cd7218c9f31d43a67c2e794e18939
Bruno Gomes <brunosoges@gmail.com>
2022-09-18 16:29:02 -0300

    learning(Overwrite): 'toString()' method override

---
+++ src/maratona/java/devdojo/orientacaoobjetos/sobrescrita/test/Aula76SobrescritaToString.java
@@ -0,0 +1,13 @@
+package maratona.java.devdojo.orientacaoobjetos.sobrescrita.test;
+
+import maratona.java.devdojo.orientacaoobjetos.sobrescrita.dominio.Desenho;
+
+public class Aula76SobrescritaToString {
+
+	public static void main(String[] args) {
+		Desenho desenho = new Desenho("Os Padrinhos Mágicos");
+
+		System.out.println(desenho);
+	}
+
+}
