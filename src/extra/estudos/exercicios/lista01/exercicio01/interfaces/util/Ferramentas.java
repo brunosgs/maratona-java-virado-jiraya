b4f07ebcb6ea73ce2ffb75f6005c57ae1485bf6e
Bruno Gomes <brunosoges@gmail.com>
2022-10-06 11:38:07 -0300

    Revert "learning(Exercises): exercise 01 Practicing object-oriented programming using the concepts of polymorphisms."
    This reverts commit e61e4e6c742a84ee31fbd4f8a6a6a12c0461abca.

--- src/extra/estudos/exercicios/lista01/exercicio01/interfaces/util/Ferramentas.java
+++
@@ -1,9 +0,0 @@
-package extra.estudos.exercicios.lista01.exercicio01.interfaces.util;
-
-import extra.estudos.exercicios.lista01.exercicio01.interfaces.Animal;
-
-public interface Ferramentas {
-	public Animal[] filtraEspecie(Animal[] completo, String especieFiltrar);
-
-	public String[] classificaEspecies(Animal[] completo);
-}
