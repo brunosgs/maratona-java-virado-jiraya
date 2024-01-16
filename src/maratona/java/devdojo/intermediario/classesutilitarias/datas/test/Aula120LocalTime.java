bdf991e956b338a219da28b11db9c37c90ad45fd
Bruno Gomes <brunosoges@gmail.com>
2024-01-16 09:25:30 -0300

    learning(localdate/localtime): working with LocalTime and LocalDate

---
+++ src/maratona/java/devdojo/intermediario/classesutilitarias/datas/test/Aula120LocalTime.java
@@ -0,0 +1,25 @@
+package maratona.java.devdojo.intermediario.classesutilitarias.datas.test;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+/**
+ * - LocalTime trabalhar só com as horas, diferente da LocalDate que trabalhar só com a data;
+ */
+public class Aula120LocalTime {
+
+	public static void main(String[] args) {
+		LocalTime ofTime = LocalTime.of(23, 32, 12);
+		LocalTime timeNow = LocalTime.now();
+
+		System.out.println("Hora personalizada: " + ofTime);
+		System.out.println("Hora atual: " + timeNow);
+		System.out.println("Horas: " + timeNow.getHour());
+		System.out.println("Minutos: " + timeNow.getMinute());
+		System.out.println("Segundos: : " + timeNow.getSecond());
+		System.out.println("Nano segundos: " + timeNow.get(ChronoField.NANO_OF_SECOND));
+		System.out.println("Hora miníma do dia: " + LocalTime.MIN);
+		System.out.println("Hora maxíma do dia: " + LocalTime.MAX);
+	}
+
+}
