45e7bc1e7ceeb500af8b689a3e881a3b5dd240b6
Bruno Gomes <brunosoges@gmail.com>
2024-01-23 09:26:46 -0300

    learning(Pattern/Matcher): beginning of regex studies with the Pattern and Matcher classes

---
+++ src/maratona/java/devdojo/Cintermediario/classesutilitarias/regex/test/Aula131RegexPatternMatcher.java
@@ -0,0 +1,35 @@
+package maratona.java.devdojo.Cintermediario.classesutilitarias.regex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * - As expressões regulares = REGEX, são formas de localizar ou substituir
+ * padrões em um texto;
+ * <p>
+ * - No exemplo 'texto2' com o regex = "aba", ao executar ele localiza somente
+ * as posições 0 e 4, quando o matcher localiza ele não retorna para cadeira de
+ * caracteres anterior, ele continua de onde parou até localizar a próxima
+ * sequencia que iguale ao que foi passado para o pattern;
+ */
+public class Aula131RegexPatternMatcher {
+
+	public static void main(String[] args) {
+		String regex = "aba";
+		String texto = "abaaba";
+		String texto2 = "abababa";
+
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(texto2);
+
+		System.out.println("Texto:  " + texto);
+		System.out.println("Indice: 0123456789");
+		System.out.println("Regex:  " + regex);
+		System.out.println("Posições encontradas");
+
+		while (matcher.find()) {
+			System.out.print(matcher.start() + " ");
+		}
+	}
+
+}
