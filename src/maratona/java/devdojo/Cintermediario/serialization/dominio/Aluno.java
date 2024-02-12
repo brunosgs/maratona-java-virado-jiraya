c421e7a9b37d3e5d542a51a26f98b1eeb568bc87
Bruno Gomes <brunosoges@gmail.com>
2024-02-12 08:22:19 -0300

    learning(Serialization): serialize and deserialize objects with transient

--- src/maratona/java/devdojo/Cintermediario/serialization/dominio/Aluno.java
+++ src/maratona/java/devdojo/Cintermediario/serialization/dominio/Aluno.java
@@ -1,15 +1,29 @@
 package maratona.java.devdojo.Cintermediario.serialization.dominio;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * - Um atributo 'transient' significa que ele não será serializado ou
+ * desserializado juntamente com os demais atributos de um determinado objeto.
+ * <p>
+ * - Atibutos estáticos não são serializados, eles pertecem a classe não ao
+ * objeto;
+ * <p>
+ * - Em caso onde não se tem acesso ao código de terceiros e precisa se
+ * serializar algo ou desserializar, se utiliza o 'transient' e depois os
+ * métodos de exemplo para read e write;
+ */
 public class Aluno implements Serializable {
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -139617710664368947L;
 	private Long id;
 	private String nome;
-	private String password;
+	private transient String password;
+	private transient Turma turma;
+	private static final String NOME_ESCOLA = "DevDojo";
 
 	public Aluno(Long id, String nome, String password) {
 		System.out.println("Contrutor Aluno");
@@ -42,9 +56,41 @@ public class Aluno implements Serializable {
 		this.password = password;
 	}
 
+	public Turma getTuma() {
+		return turma;
+	}
+
+	public void setTuma(Turma turma) {
+		this.turma = turma;
+	}
+
+	@Serial
+	private void writeObject(ObjectOutputStream oos) {
+		try {
+			oos.defaultWriteObject();
+			oos.writeUTF(turma.getNome());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Serial
+	private void readObject(ObjectInputStream ois) {
+		try {
+			// Para que seja feita a leitura, deve ser na mesma ordem que foi escrito.
+			ois.defaultReadObject();
+
+			String nomeTurma = ois.readUTF();
+			turma = new Turma(nomeTurma);
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public String toString() {
-		return "Aluno [id=" + id + ", nome=" + nome + ", password=" + password + "]";
+		return "Aluno [id=" + id + ", nome=" + nome + ", password=" + password + ", NOME_ESCOLA=" + NOME_ESCOLA
+				+ ", turma=" + turma + "]";
 	}
 
 }
---
+++ src/maratona/java/devdojo/Cintermediario/serialization/test/Aula160Serialization.java
@@ -0,0 +1,54 @@
+package maratona.java.devdojo.Cintermediario.serialization.test;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import maratona.java.devdojo.Cintermediario.serialization.dominio.Aluno;
+import maratona.java.devdojo.Cintermediario.serialization.dominio.Turma;
+
+/**
+ * - Muitos frameworks utilizam esse conceito;
+ * <p>
+ * - Quando se esta utilizando a serialização, esta transformando o objeto em um
+ * array de bytes;
+ */
+public class Aula160Serialization {
+
+	public static void main(String[] args) {
+		Aluno aluno = new Aluno(100L, "Teste nome", "!$#%$#VGgfdsf");
+		Turma turma = new Turma("Maratona Java Virado no Jiraya");
+
+		aluno.setTuma(turma);
+
+		serializar(aluno);
+		deserializar();
+	}
+
+	private static void serializar(Aluno aluno) {
+		Path fileSerialization = Paths.get("pasta_diretorio/aluno.ser");
+
+		try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(fileSerialization))) {
+			oos.writeObject(aluno);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private static void deserializar() {
+		Path fileSerialization = Paths.get("pasta_diretorio/aluno.ser");
+
+		try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(fileSerialization))) {
+
+			Aluno aluno = (Aluno) ois.readObject();
+
+			System.out.println(aluno);
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
