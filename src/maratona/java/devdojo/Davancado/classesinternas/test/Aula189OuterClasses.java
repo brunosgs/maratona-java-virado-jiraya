47cbf2beda766187755f02ad1fddd02102a92214
Bruno Gomes <brunosoges@gmail.com>
2024-03-12 22:40:34 -0300

    refactor: adjusting class name

--- src/maratona/java/devdojo/Davancado/classesinternas/test/Aula189OuterClasses.java
+++ src/maratona/java/devdojo/Davancado/classesinternas/test/Aula189OuterClasses.java
@@ -7,22 +7,23 @@ package maratona.java.devdojo.Davancado.classesinternas.test;
  * - Quando se chama o 'this', ele vai sempre se referenciar ao contexto da
  * classe que ele esta sendo chamado;
  */
-public class Aula189InnerClasses {
+public class Aula189OuterClasses {
 	private String name = "Monkey D. Luffy";
 
+	// Nested
 	class Inner {
 		public void printOuterClassAttributes() {
 			System.out.println(name);
 			System.out.println(this);
-			System.out.println(Aula189InnerClasses.this);
+			System.out.println(Aula189OuterClasses.this);
 		}
 	}
 
 	public static void main(String[] args) {
-		Aula189InnerClasses aula189 = new Aula189InnerClasses();
+		Aula189OuterClasses aula189 = new Aula189OuterClasses();
 		Inner outer = aula189.new Inner();
 
-		Inner outer2 = new Aula189InnerClasses().new Inner();
+		Inner outer2 = new Aula189OuterClasses().new Inner();
 
 		outer.printOuterClassAttributes();
 		outer2.printOuterClassAttributes();
