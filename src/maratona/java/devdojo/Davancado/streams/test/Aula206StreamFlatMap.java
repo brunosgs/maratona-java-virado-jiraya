5ec642d62959a37a3e89725c772ea07a9266f004
Bruno Gomes <brunosoges@gmail.com>
2024-03-26 08:02:00 -0300

    streams
    learning: flattening lists of lists to a single list

---
+++ src/maratona/java/devdojo/Davancado/streams/test/Aula206StreamFlatMap.java
@@ -0,0 +1,36 @@
+package maratona.java.devdojo.Davancado.streams.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aula206StreamFlatMap {
+
+	public static void main(String[] args) {
+		List<List<String>> devdojo = new ArrayList<>();
+		List<String> graphicDesigners = List.of("Pedro", "João", "Maria", "José", "Roberto");
+		List<String> developers = List.of("Gustavo", "William", "Mario", "Patricia", "Ana");
+		List<String> students = List.of("Édipo", "Guto", "Roberta", "Cesar", "Clara");
+
+		devdojo.add(graphicDesigners);
+		devdojo.add(developers);
+		devdojo.add(students);
+
+		// Leitura de todos os nomes sem stream
+		for (List<String> people : devdojo) {
+			for (String person : people) {
+				System.out.println(person);
+			}
+		}
+
+		System.out.println("------------------- x -------------------");
+
+		/**
+		 * Ao utlizar o flatMap, esta achatando = flatten a lista de lista para uma
+		 * lista unica;
+		 */
+		devdojo.stream()
+				.flatMap(List::stream)
+				.forEach(System.out::println);
+	}
+
+}
