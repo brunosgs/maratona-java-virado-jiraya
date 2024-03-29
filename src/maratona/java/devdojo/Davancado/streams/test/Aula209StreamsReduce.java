055f451ba7f8061318ee4db8d823d5200b5c0ab8
Bruno Gomes <brunosoges@gmail.com>
2024-03-29 06:55:48 -0300

    streams
    learning: using reduction with numeric types

---
+++ src/maratona/java/devdojo/Davancado/streams/test/Aula209StreamsReduce.java
@@ -0,0 +1,35 @@
+package maratona.java.devdojo.Davancado.streams.test;
+
+import java.util.List;
+
+/**
+ * - 'Reduce' é um acumulador, onde o retorno sempre será um unico resultado;
+ */
+public class Aula209StreamsReduce {
+
+	public static void main(String[] args) {
+		// Reduce utilizando valores númericos
+		List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+
+		integers.stream()
+				.reduce((a, b) -> a + b)
+				.ifPresent(System.out::println);
+
+		Integer integer = integers.stream()
+				.reduce(0, (a, b) -> a + b);
+
+		System.out.println(integer);
+
+		Integer integer2 = integers.stream()
+				.reduce(Integer::sum)
+				.get();
+
+		System.out.println(integer2);
+
+		System.out.println("-------------------- x --------------------");
+		integers.stream()
+				.reduce(Integer::max)
+				.ifPresent(System.out::println);
+	}
+
+}
