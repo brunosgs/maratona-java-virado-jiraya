aa364a588695b2d50e6928b18a77647ed925cdcc
brunosgs <brunosoges@gmail.com>
2024-11-08 07:42:53 -0300

    jdbc
    learning: configuring factory for database access

---
+++ src/maratona/java/devdojo/Ejdbc/conn/ConnectionFactory.java
@@ -0,0 +1,12 @@
+package maratona.java.devdojo.Ejdbc.conn;
+
+/**
+ * - O JDBC (Java Database Connectivity) é uma API (Application Programming
+ * Interface) do Java que permite a conexão e interação com bancos de dados
+ * relacionais.
+ * <p>
+ * - java.sql = Connection, Statement, ResultSet e DriverManager
+ */
+public class ConnectionFactory {
+
+}
