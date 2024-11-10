d76a49024002c14f38d05e52411a601d56605706
brunosgs <brunosoges@gmail.com>
2024-11-10 11:39:41 -0300

    jdbc
    learning: include dependencies in maven and creating the first database connection.

--- src/maratona/java/devdojo/Ejdbc/conn/ConnectionFactory.java
+++ src/maratona/java/devdojo/Ejdbc/conn/ConnectionFactory.java
@@ -1,5 +1,9 @@
 package maratona.java.devdojo.Ejdbc.conn;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * - O JDBC (Java Database Connectivity) é uma API (Application Programming
  * Interface) do Java que permite a conexão e interação com bancos de dados
@@ -8,5 +12,19 @@ package maratona.java.devdojo.Ejdbc.conn;
  * - java.sql = Connection, Statement, ResultSet e DriverManager
  */
 public class ConnectionFactory {
+	public static Connection getConnection() {
+		String url = "jdbc:mysql://localhost:3306/anime_store";
+		String username = "root";
+		String password = "root";
+		Connection conn = null;
 
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+			System.out.println(conn);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
 }
---
+++ src/maratona/java/devdojo/Ejdbc/test/Aula256ConnectionJDBC.java
@@ -0,0 +1,11 @@
+package maratona.java.devdojo.Ejdbc.test;
+
+import maratona.java.devdojo.Ejdbc.conn.ConnectionFactory;
+
+public class Aula256ConnectionJDBC {
+
+	public static void main(String[] args) {
+		ConnectionFactory.getConnection();
+	}
+
+}
